75ca0058af464b322aad566403db4beb2ccc8f48
Mayank Goyal <mayankgoyal.mait@gmail.com>
2026-02-13 22:55:37 +0530

    firesbase itntegration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swift_ai/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: SwiftAIApp()));
 }
 
